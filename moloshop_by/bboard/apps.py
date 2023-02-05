913198a17caab2dac69abc100207962bef8127f9
elvand967 <vladimir.dubanevich@gmail.com>
2023-02-05 18:10:59 +0300

    String representation of the model

--- moloshop_by/bboard/apps.py
+++ moloshop_by/bboard/apps.py
@@ -1,6 +1,8 @@
-from django.apps import AppConfig
+# D:\MEGAsync\PycharmProjects\BulletinBoard\moloshop_by\bboard\apps.py
 
+from django.apps import AppConfig
 
 class BboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bboard'
+    verbose_name = 'Доска объявлений'
--- moloshop_by/bboard/models.py
+++ moloshop_by/bboard/models.py
@@ -7,8 +7,21 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='Oпиcaниe')  # сам текст объявления, описание товара (тип — memo);
     price = models.FloatField(null=True, blank=True, verbose_name='Цeнa')  # цена (тип — вещественное число);
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')  # дата публикации (тип — временнáя отметка, значение по умолчанию — текущие дата и время, индексированное).
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
     class Meta:
         verbose_name_plural = 'Объявления'
         verbose_name= 'Объявление'
         ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Haзвaниe')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
