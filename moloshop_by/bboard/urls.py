4c1cb986c580b5b615d0283131c03ca4e9c525fe
elvand967 <vladimir.dubanevich@gmail.com>
2023-02-06 16:46:04 +0300

    Reverse Internet address resolution

--- moloshop_by/bboard/urls.py
+++ moloshop_by/bboard/urls.py
@@ -1,8 +1,9 @@
 # D:\MEGAsync\PycharmProjects\BulletinBoard\moloshop_by\bboard\urls.py
 
 from django.urls import path
-from .views import index
+from .views import index, by_rubric
 
 urlpatterns = [
-    path('', index),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', index, name='index'),
 ]
